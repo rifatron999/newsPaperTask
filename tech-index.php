8aa4978b5cc2487af6e9a6ba3d7276c7650ec978
Kawsarul Alam Chowdhury Rifat <rifatron999@gmail.com>
2020-10-23 17:28:35 +0600

    google signin 01

--- tech-index.php
+++ tech-index.php
@@ -1,5 +1,6 @@
 <?php
 require('api.php');
+require('index.php');
 ?>
 
 <!DOCTYPE html>
@@ -68,6 +69,21 @@ require('api.php');
                         <ul class="navbar-nav mr-2">
                             <li class="nav-item"><!--google signin will be here-->
                                 <a class="nav-link" href="#"><i class="fa fa-apple"></i></a>
+                                <?php
+   if($login_button == '')
+   {
+    echo '<div class="panel-heading">Welcome User</div><div class="panel-body">';
+    echo '<img src="'.$_SESSION["user_image"].'" class="img-responsive img-circle img-thumbnail" />';
+    echo '<h3><b>Name :</b> '.$_SESSION['user_first_name'].' '.$_SESSION['user_last_name'].'</h3>';
+    echo '<h3><b>Email :</b> '.$_SESSION['user_email_address'].'</h3>';
+    echo '<h3><a href="logout.php">Logout</h3></div>';
+   }
+   else
+   {
+    echo '<div align="center">'.$login_button . '</div>';
+   }
+   ?>
+
                             </li>
                         </ul>
                     </div>
